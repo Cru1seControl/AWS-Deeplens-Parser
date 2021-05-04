4e87df7c426dc44a07a0f42e6d86da99adcd34a3
Xander Tedder <cru1secontrol.loot@gmail.com>
2021-05-04 13:53:11 -0400

    Update averages.py

--- examples/averages.py
+++ examples/averages.py
@@ -2,14 +2,14 @@ from deeplens import deeplens
 from datetime import datetime
 
 with open("json/face_dict.json", "r") as j:
-	j = j.read()
+    j = j.read()
 
 deeplens = deeplens(j)
 nonprecise = deeplens.face(precise=False)
 
 print("Recorded Times %d" % (len(nonprecise)))
 for stamp in deeplens.timestamps:
-	print(datetime.fromtimestamp(stamp // 1000.0))
+    print(datetime.fromtimestamp(stamp // 1000.0))
 
 print("min: %f, max: %f" % (deeplens.min(False), deeplens.max(False)))
 print("Average: %f\n" % (deeplens.avg))

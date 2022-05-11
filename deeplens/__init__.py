d4685ba0515847955cf328754c2c77e6fcc65d9a
wnstdrr <xandertedder7@gmail.com>
2022-05-11 09:10:28 -0400

    update __init__ method

--- deeplens/__init__.py
+++ deeplens/__init__.py
@@ -1,2 +1,10 @@
-from .deeplens import deeplens
+from deeplens.deeplens import *
+
+__author__ = "Xander Tedder (xandertedder7@gmail.com)"
+__version__ = "0.0.1"
+
+
+__name__ = "deeplens"
+__package__ = "deeplens"
+
 __all__ = ["qos", "topic", "format", "timestamps", "avg", "min", "max", "face"]

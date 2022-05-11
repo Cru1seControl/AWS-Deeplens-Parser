231a5c1345c00c2484023b7d011a55e668072514
wnstdrr <xandertedder7@gmail.com>
2022-05-11 10:47:42 -0400

    add __name__, __package__

--- deeplens/__init__.py
+++ deeplens/__init__.py
@@ -4,4 +4,8 @@ __author__ = "Xander Tedder (xandertedder7@gmail.com)"
 __version__ = "0.0.1"
 __license__ = "GNU GPL v3.0"
 
-__all__ = ["deeplens"]
+
+__name__ = "deeplens"
+__package__ = "deeplens"
+
+__all__ = ["dlens"]
